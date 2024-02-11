9c68cfeab14d2a0a37a608d4cecbc1acf5764f6b
KoSik <kosik84@gmail.com>
2024-02-11 10:33:50 +0100

    clean

--- Core/Inc/tsl2561.h
+++ Core/Inc/tsl2561.h
@@ -1,9 +1,13 @@
 /*
- * gy2561.h
+ * tsl2561.h
  *
- *  Created on: 19 maj 2019
- *      Author: kosik
+ *  Created on: 10.02.2024
+ *      Author: Marcin Kosela (KoSik)
+ *		e-mail: kosik84@gmail.com
+ *
+ *	   version: 1.0
  */
+
 #include "main.h"
 #include <stdint.h>
 #include <string.h>
@@ -19,8 +23,8 @@
 #define TSL2561_REGISTER_THRESHHOLDH_LOW  (0x04) // Interrupt high threshold low-byte
 #define TSL2561_REGISTER_THRESHHOLDH_HIGH (0x05) // Interrupt high threshold high-byte
 #define TSL2561_REGISTER_INTERRUPT 		  (0x06) // Interrupt settings
-#define TSL2561_REGISTER_CRC 			  (0x08) // Factory use only
-#define TSL2561_REGISTER_ID 			  (0x0A) // #define GY2561 identification setting
+#define TSL2561_REGISTER_CRC 			  (0x08)
+#define TSL2561_REGISTER_ID 			  (0x0A) // Identification register
 #define TSL2561_REGISTER_CHAN0_LOW 		  (0x0C) // Light data channel 0, low byte
 #define TSL2561_REGISTER_CHAN0_HIGH       (0x0D) // Light data channel 0, high byte
 #define TSL2561_REGISTER_CHAN1_LOW 		  (0x0E) // Light data channel 1, low byte
@@ -28,8 +32,8 @@
 
 #define TSL2561_COMMAND_BIT       		  (0x80)
 #define TSL2561_CLEAR_BIT         		  (0x40) //< Clears pending interrupt
-#define TSL2561_WORD_BIT          		  (0x20) //< 1 = read/write word
-#define TSL2561_BLOCK_BIT         		  (0x10) //< 1 = using block read/write
+#define TSL2561_WORD_BIT          		  (0x20)
+#define TSL2561_BLOCK_BIT         		  (0x10)
 
 #define TSL2561_ON 						  (0x03)
 #define TSL2561_OFF 					  (0x00)
@@ -41,10 +45,12 @@
 #define TSL2561_INTEG_402					  (0x02)
 #define TSL2561_INTEG_OFF					  (0x03)
 
+
 struct TSL2561 {
 	I2C_HandleTypeDef *hi2c;
 	uint8_t sensorAddress;
 };
+
 
 HAL_StatusTypeDef tsl2561_init(I2C_HandleTypeDef *hi2c, uint8_t sensorAddress, uint8_t integ);
 float tsl2561_readLux(void);
